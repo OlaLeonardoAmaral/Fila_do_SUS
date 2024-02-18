89763a6bb482f4aa123c9d0202e3be2d10d7b713
Leonardo <leonardo3442@hotmail.com>
2024-02-18 00:38:57 -0300

    Estrutura inicial do backend

---
+++ backend/src/main/java/br/com/impacta/backend/FiladosusApplication.java
@@ -0,0 +1,13 @@
+package br.com.impacta.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiladosusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiladosusApplication.class, args);
+	}
+
+}
