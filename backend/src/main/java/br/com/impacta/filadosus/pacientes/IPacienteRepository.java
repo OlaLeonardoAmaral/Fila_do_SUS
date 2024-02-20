312f12aa9e07ffe963ae95d4340686dd507394d9
Leonardo <leonardo3442@hotmail.com>
2024-02-19 23:46:20 -0300

    Funcionalidades Buscar todos pacientes, buscar por nome e por cpf implementadas

--- backend/src/main/java/br/com/impacta/filadosus/pacientes/IPacienteRepository.java
+++ backend/src/main/java/br/com/impacta/filadosus/pacientes/IPacienteRepository.java
@@ -1,7 +1,10 @@
 package br.com.impacta.filadosus.pacientes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface IPacienteRepository extends JpaRepository<PacienteEntity, Integer>{
-    
+    List<PacienteEntity> findPacienteByNomeContainingIgnoreCase(String nome);
+    PacienteEntity findPacienteByCpf(String cpf);
 }
--- backend/src/main/java/br/com/impacta/filadosus/pacientes/PacienteController.java
+++ backend/src/main/java/br/com/impacta/filadosus/pacientes/PacienteController.java
@@ -2,23 +2,50 @@ package br.com.impacta.filadosus.pacientes;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 @RequestMapping("/paciente")
+@CrossOrigin(origins = "*")
 public class PacienteController {
  
     @Autowired
-    IPacienteRepository pessoaRepository;
+    IPacienteRepository pacienteRepository;
 
 
-    @PostMapping("/add")
+    @PostMapping("/adicionar")
     public ResponseEntity<PacienteEntity> create(@RequestBody PacienteEntity pessoa) {
         pessoa.setStatus("EM ESPERA");
-        var pessoaCreated = pessoaRepository.save(pessoa);
+        var pessoaCreated = pacienteRepository.save(pessoa);
         return ResponseEntity.ok().body(pessoaCreated);
     }
+
+    @GetMapping("/")
+    public ResponseEntity<?> list() {
+        var pacientes = pacienteRepository.findAll();
+        return ResponseEntity.ok().body(pacientes);
+    }
+
+    @GetMapping("/nome/{nome}")
+    public ResponseEntity<?> findByNome(@PathVariable String nome) {
+        var paciente = pacienteRepository.findPacienteByNomeContainingIgnoreCase(nome);
+        return ResponseEntity.ok().body(paciente);
+    }
+
+    @GetMapping("/cpf/{cpf}")
+    public ResponseEntity<?> findByCpf(@PathVariable String cpf) {
+        var teste = pacienteRepository.findPacienteByCpf(cpf);
+        return ResponseEntity.ok().body(teste);
+    }
+    
+    
+    
 }
