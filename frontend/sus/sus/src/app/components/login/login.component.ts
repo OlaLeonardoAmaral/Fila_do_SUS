eb3bf4c4947f173e6f70e099103088d403b09627
Geovanna Anjos <g.dos.santos.anjos@avanade.com>
2024-03-08 16:59:34 -0300

    Responsividade do formulário 'login' e demais componentes

--- frontend/sus/sus/src/app/components/login/login.component.ts
+++ frontend/sus/sus/src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
@@ -8,6 +8,12 @@ import { HeaderComponent } from '../header/header.component';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  toggleForm() {
+    const form = document.querySelector('form');
+    if (form) {
+      form.animate({ height: 'toggle', opacity: 'toggle' });
+    }
+  }
 }
